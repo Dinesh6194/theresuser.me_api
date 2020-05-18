2a70f07aa838708c0c75fd74995951c91725db2c
Dinesh6194 <dkar0013@student.monash.edu>
2020-05-18 17:48:06 +1000

    API changes for iteration 2

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask, request, jsonify
 from flask_restplus import Resource, Api, fields
 from database import db_session
-from models import BlogPost,Item,ItemCategory,Year,Color,Postitem,Carbon
+from models import BlogPost,Item_details,Year,Color,Postitem,Carbon, UserActivity, UserDetails
+from sqlalchemy import func, extract
 import json
+import numpy as np
+import pandas as pd
+import shapefile as shp
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
+import utm
+
 application = Flask(__name__)
 api = Api(application,
           version='0.1',
@@ -39,7 +47,7 @@ class PostMessage(Resource):
 class PutMessage(Resource):
     def put(self):
         data=request.get_json()
-        print(data)
+        
         # datatoupdate={BlogPost.post:data['post']}
         postdata=BlogPost.query.filter_by(title=data['title']).first()
         postdata.post = data['post']
@@ -90,7 +98,7 @@ class Years(Resource):
 class Postitems(Resource):
     def post(self):
         data=request.get_json()
-        post=Postitem(post_id=data['post_id'],item_id=data['item_id'],type_id=data['type_id'],color_id=data['color_id'],year_id=data['year_id'],latitude=data['latitude'],longitude=data['longitude'],status=1)
+        post=Postitem(post_id=data['post_id'],item_id=data['item_id'],color_id=data['color_id'],year_id=data['year_id'],latitude=data['latitude'],longitude=data['longitude'],status=1)
         db_session.add(post)
         db_session.commit()
         return {'Message':'Success'}
@@ -98,13 +106,11 @@ class Postitems(Resource):
 @api.route('/allinone')
 class Allinone(Resource):
     def get(self, **kwargs):
-        item=list(map(lambda it: it.serialize(), Item.query.all()))
-        types=list(map(lambda ty: ty.serialize(), ItemCategory.query.all()))
+        item=list(map(lambda it: it.serialize(), Item_details.query.all()))
         colors=list(map(lambda cl: cl.serialize(), Color.query.all()))
         years=list(map(lambda yr: yr.serialize(), Year.query.all()))
         valuedict={}
         valuedict["Item"]=item
-        valuedict["ItemCategory"]=types
         valuedict["Color"]=colors
         valuedict['Year']=years
         
@@ -116,7 +122,7 @@ class Postitems(Resource):
    
     def get(self, **kwargs):
 
-        fetchedvalues=(db_session.query(Postitem,Item,ItemCategory,Color,Year).join(Item).join(ItemCategory).join(Color).join(Year)).filter(Postitem.status==1).all()
+        fetchedvalues=(db_session.query(Postitem,Item_details,Color,Year).join(Item_details).join(Color).join(Year)).filter(Postitem.status==1).all()
         postlist=[]
         for listelement in fetchedvalues:
             valuedict={}
@@ -131,7 +137,7 @@ class Postitems(Resource):
    
     def post(self, **kwargs):
         data=request.get_json()
-        fetchedvalues=(db_session.query(Postitem,Item,ItemCategory,Color,Year).join(Item).join(ItemCategory).join(Color).join(Year)).filter(Item.item_name==data['item_name'],Postitem.status==1).all()
+        fetchedvalues=(db_session.query(Postitem,Item_details,Color,Year).join(Item_details).join(Color).join(Year)).filter(Item_details.item_name==data['item_name'],Postitem.status==1).all()
         postlist=[]
         for listelement in fetchedvalues:
             valuedict={}
@@ -144,7 +150,7 @@ class Postitems(Resource):
 class Pickitem(Resource):
     def post(self):
         data=request.get_json()
-        print(data)
+        
         # datatoupdate={BlogPost.post:data['post']}
         postdata=Postitem.query.filter_by(post_id=data['post_id']).first()
         postdata.status = 0
@@ -156,7 +162,7 @@ class Postitems(Resource):
    
     def get(self, **kwargs):
 
-        fetchedvalues=(db_session.query(Postitem,Item,ItemCategory,Color,Year).join(Item).join(ItemCategory).join(Color).join(Year)).all()
+        fetchedvalues=(db_session.query(Postitem,Item_details,Color,Year).join(Item_details).join(Color).join(Year)).all()
         postlist=[]
         for listelement in fetchedvalues:
             valuedict={}
@@ -175,6 +181,104 @@ class Carbons(Resource):
         fetchedvalues=list(map(lambda c: c.serialize(),db_session.query(Carbon).filter(Carbon.item_name==data['item_name']).all()))
         
         return jsonify(fetchedvalues)
+
+@api.route('/postedactivities')
+class UserActivities(Resource):
+   
+    def post(self, **kwargs):
+        data=request.get_json()
+        fetchedvalues=(db_session.query(UserActivity,Postitem,Item_details).filter(UserActivity.post_id==Postitem.post_id,Postitem.item_id==Item_details.item_id,UserActivity.user_email==data['user_email']).all())
+        postlist=[]
+        for listelement in fetchedvalues:
+            valuedict={}                  
+            for tupleelement in listelement:
+                valuedict.update(tupleelement.serialize())
+            postlist.append(valuedict)
+       
+        return jsonify(postlist)
+@api.route('/post_useractivity')
+class UserActivitys(Resource):
+    def post(self):
+        request_data=request.get_json()
+        list_data=list(request_data)
+        for data in list_data:
+            useractivity=UserActivity(record_id=data['record_id'],user_email=data['user_email'],post_id=data['post_id'],contributed_date=data['contributed_date'],activity_category=data['activity_category'])
+            db_session.add(useractivity)
+            db_session.commit()
+        return {'Message':'Success'}
+
+@api.route('/post_user')
+class UserDetails(Resource):
+    def post(self):
+        data=request.get_json()
+        user=UserDetails(user_email=data['user_email'],user_name=data['user_name'])
+        db_session.add(user)
+        db_session.commit()
+        return {'Message':'Success'}
+
+@api.route('/user_name')
+class UserDetailss(Resource):
+   
+    def post(self, **kwargs):
+        data=request.get_json()
+        fetchedvalues=list(map(lambda c: c.serialize(),db_session.query(UserDetails).filter(UserDetails.user_email==data['user_email']).all()))
+        
+        return jsonify(fetchedvalues)
+
+
+@api.route('/post_items_list')
+class Postitems(Resource):
+    def post(self):
+        data=request.get_json()
+        list_data=list(data)
+        for i in list_data:
+            post=Postitem(post_id=i['post_id'],item_id=i['item_id'],color_id=i['color_id'],year_id=i['year_id'],latitude=i['latitude'],longitude=i['longitude'],status=1)
+            db_session.add(post)
+            db_session.commit()
+        return {'Message':'Success'}
+
+@api.route('/allactivity')
+class UserActivityss(Resource):
+    def get(self, **kwargs):
+        user_activity=list(map(lambda it: it.serialize(), UserActivity.query.all()))     
+        return jsonify(user_activity)
+
+@api.route('/top_three')
+class TopUserActivities(Resource):
+   
+    def post(self, **kwargs):
+        data=request.get_json()
+        fetchedvalues=db_session.query(UserActivity,Postitem,Item_details).filter(UserActivity.post_id==Postitem.post_id,Postitem.item_id==Item_details.item_id,extract('Month',UserActivity.contributed_date)==data['month']).group_by(UserActivity.user_email).with_entities(UserActivity.user_email,func.sum(Item_details.carbon_intensity*Item_details.kg),func.sum(Item_details.kg)).order_by(func.sum(Item_details.carbon_intensity*Item_details.kg).desc()).limit(3).all()
+        fetcheduservalues=db_session.query(UserActivity,Postitem,Item_details).filter(UserActivity.post_id==Postitem.post_id,Postitem.item_id==Item_details.item_id,extract('Month',UserActivity.contributed_date)==data['month'],UserActivity.user_email==data['user_email']).group_by(UserActivity.user_email).with_entities(UserActivity.user_email,func.sum(Item_details.carbon_intensity*Item_details.kg),func.sum(Item_details.kg)).all()
+        postlist=[]
+        for valuelist in fetchedvalues:
+            valuedict={}
+            valuedict["user_email"]=valuelist[0]
+            valuedict["total_ci"]=valuelist[1]
+            valuedict["total_weight"]=valuelist[2]
+            postlist.append(valuedict)
+        for valuelist in fetcheduservalues:
+            valuedict={}
+            valuedict["user_email"]=valuelist[0]
+            valuedict["total_ci"]=valuelist[1]
+            valuedict["total_weight"]=valuelist[2]
+            postlist.append(valuedict)
+        return jsonify(postlist)
+
+@api.route('/day') 
+class Location(Resource):       
+    def post(self,**kwargs):
+        data=request.get_json()
+        tests = utm.from_latlon(data['latitude'], data['longitude'])
+        day='None'
+        shp_path = 'domesticwastedays/DOMESTIC_WASTE_DAYS.shp'
+        sf = shp.Reader(shp_path)
+        point = Point(tests[0], tests[1])
+        for i in range(0,len(sf.records())):
+            polygon = Polygon(sf.shapeRecords()[i].shape.points[:])
+            if(polygon.contains(point)):
+                day=sf.records()[i][0]
+        return {"day":day}
 
 
 @application.teardown_appcontext
--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Table, Column, Integer, Text, Float, ForeignKey
+from sqlalchemy import Table, Column, Integer, Text, Float, ForeignKey, Date
 from sqlalchemy.orm import mapper
 from database import metadata, db_session
 
@@ -18,42 +18,29 @@ blog_posts = Table('blog_posts', metadata,
 mapper(BlogPost, blog_posts)
 
 
-class Item(object):
+class Item_details(object):
     query = db_session.query_property()
-    def __init__(self, item_id=None, item_name=None):
+    def __init__(self, item_id=None, item_name=None,carbon_intensity=None,type_name=None,kg=None):
         self.item_id = item_id
         self.item_name = item_name
+        self.carbon_intensity=carbon_intensity
+        self.type_name=type_name
+        self.kg=kg
 
     def serialize(self):
-     	return {"item_id":self.item_id,"item_name":self.item_name}
+     	return {"item_id":self.item_id,"item_name":self.item_name,"carbon_intensity":self.carbon_intensity,"type_name":self.type_name,"kg":self.kg}
         
 
-items = Table('items', metadata,
+items = Table('item_details', metadata,
     Column('item_id', Integer, primary_key=True),
-    Column('item_name', Text)
+    Column('item_name', Text),
+    Column('type_name', Text),
+    Column('carbon_intensity',Float),
+    Column('kg',Float)
     
 )
 
-mapper(Item, items)
-
-
-class ItemCategory(object):
-    query = db_session.query_property()
-    def __init__(self, type_id=None, type_name=None):
-        self.type_id = type_id
-        self.type_name = type_name
-
-    def serialize(self):
-     	return {"type_id":self.type_id,"type_name":self.type_name}
-        
-
-item_category = Table('item_category', metadata,
-    Column('type_id', Integer, primary_key=True),
-    Column('type_name', Text)
-    
-)
-
-mapper(ItemCategory, item_category)
+mapper(Item_details, items)
 
 
 class Color(object):
@@ -96,10 +83,9 @@ mapper(Year, years)
 
 class Postitem(object):
     query = db_session.query_property()
-    def __init__(self, post_id=None,item_id=None,type_id=None,color_id=None,year_id=None,latitude=None,longitude=None,status=None):
+    def __init__(self, post_id=None,item_id=None,color_id=None,year_id=None,latitude=None,longitude=None,status=None):
         self.post_id=post_id
         self.item_id=item_id
-        self.type_id=type_id
         self.color_id=color_id
         self.year_id=year_id
         self.latitude=latitude
@@ -107,13 +93,12 @@ class Postitem(object):
         self.status=status
 
     def serialize(self):
-     	return {"post_id":self.post_id,"item_id":self.item_id,"type_id":self.type_id,"color_id":self.color_id,"year_id":self.year_id,"latitude":self.latitude,"longitude":self.longitude,"status":self.status}
+     	return {"post_id":self.post_id,"item_id":self.item_id,"color_id":self.color_id,"year_id":self.year_id,"latitude":self.latitude,"longitude":self.longitude,"status":self.status}
         
 
 posts = Table('posts', metadata,
     Column('post_id', Integer, primary_key=True),
-    Column('item_id', Integer, ForeignKey(Item.item_id)),
-    Column('type_id', Integer,ForeignKey(ItemCategory.type_id)),
+    Column('item_id', Integer, ForeignKey(Item_details.item_id)),
     Column('color_id', Integer,ForeignKey(Color.color_id)),
     Column('year_id', Integer,ForeignKey(Year.year_id)),
     Column('latitude', Float),
@@ -144,3 +129,50 @@ carbons = Table('carbonintensity', metadata,
 )
 
 mapper(Carbon, carbons)
+
+class UserDetails(object):
+    query = db_session.query_property()
+    def __init__(self,user_email=None,user_name=None):
+        self.user_email = user_email
+        self.user_name = user_name
+        
+
+    def serialize(self):
+     	return {"user_email":self.user_email,"user_name":self.user_name}
+        
+
+userDetails = Table('user_details', metadata,
+    Column('user_email', Text, primary_key=True),
+    Column('user_name', Text)
+    
+    
+)
+
+mapper(UserDetails, userDetails)
+
+class UserActivity(object):
+    query = db_session.query_property()
+    def __init__(self,record_id=None,user_email=None,post_id=None,contributed_date=None,activity_category=None):
+    	self.record_id=record_id
+    	self.user_email = user_email
+    	self.contributed_date = contributed_date
+    	self.post_id=post_id
+    	self.activity_category=activity_category
+        
+
+    def serialize(self):
+     	return {"record_id":self.record_id,"user_email":self.user_email,"post_id":self.post_id,"contributed_date":self.contributed_date,"activity_category":self.activity_category}
+        
+
+userActivity = Table('user_activity', metadata,
+	Column('record_id', Integer, primary_key=True),
+    Column('user_email', Text),
+    Column('post_id', Integer,ForeignKey(Postitem.post_id)),
+    Column('contributed_date',Date),
+    Column('activity_category',Integer)
+    
+    
+)
+
+mapper(UserActivity, userActivity)
+
